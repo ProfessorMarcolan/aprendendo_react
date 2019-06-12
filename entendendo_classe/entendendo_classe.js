82ab0f6479358ea53bf5ca45aee32e728bbc8cdd
jmarcolan <jamarcolan@gmail.com>
2019-06-11 20:44:21 -0700

    Reorganizando o repozitorio. Escrevendo um programa que roda o react usando herança.

---
+++ entendendo_classe/entendendo_classe.js
@@ -0,0 +1,12 @@
+// https://reactjs.org/docs/react-without-jsx.html
+
+class Hello extends React.Component {
+    render() {
+      return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+    }
+  }
+  let div = React.createElement(Hello, {toWhat: 'World'}, null)
+  ReactDOM.render(
+    div,
+    document.getElementById('root')
+  );
